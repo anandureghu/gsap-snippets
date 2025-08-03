5b5cd5af3d65d10786e18a77227c668282db35a9
anandureghu <anandukr145@gmail.com>
2025-08-03 19:16:32 +0530

    updated header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,56 @@
+import Image from "next/image";
 import React from "react";
+import GlimpseIcon from "@/icons/glimpse.svg";
 
 const Header = () => {
   return (
-    <div className="h-screen flex items-center justify-center text-5xl">
-      GSAP Learning
+    <div className="h-screen flex items-center justify-center flex-col text-center gap-3">
+      <h1 className="text-5xl font-semibold text-gd-secondary">
+        GSAP SNIPPETS
+      </h1>
+      <h3 className="text-xl max-w-1/2 opacity-70 lowercase">
+        Curated collection of{" "}
+        <b className="uppercase font-semibold text-green-400">GSAP</b> snippets
+        collected while learning 😬
+      </h3>
+      <p className="text-sm text-white/60 flex items-center gap-1">
+        you can find the source code from{" "}
+        <a
+          href="https://github.com/anandureghu/gsap-snippets/tree/main/src/sections"
+          className="text-gd-highlight hover:underline max-w-3/4"
+          target="_blank"
+        >
+          @gsap-snippets
+        </a>{" "}
+        {/* <Image
+          src={GlimpseIcon}
+          width={10}
+          height={10}
+          className="grayscale"
+          alt="glipse-icon"
+        /> */}
+        by
+        <a
+          href="https://anandureghu.vercel.app"
+          className="text-gd-secondary hover:underline font-medium"
+          target="_blank"
+        >
+          anandureghu
+        </a>{" "}
+        github repo
+      </p>
+
+      <p className="text-xs text-white/60 flex items-center gap-1">
+        stay tuned{" "}
+        <Image
+          src={GlimpseIcon}
+          width={10}
+          height={10}
+          className="grayscale"
+          alt="glipse-icon"
+        />
+        more updates are coming
+      </p>
     </div>
   );
 };
