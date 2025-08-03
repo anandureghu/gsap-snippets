7035cb79e120e2d653fbb0e10d4d07a09513315c
anandureghu <anandukr145@gmail.com>
2025-08-03 18:52:15 +0530

    implemented horizontal scrollbar

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import Header from "../sections/Header";
-import Typewriter from "../sections/Typewriter";
+import Header from "@/sections/Header";
+import HorizontalScroll from "@/sections/HorizontalScroll";
+import Typewriter from "@/sections/Typewriter";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Typewriter />
+      <HorizontalScroll />
     </>
   );
 }
