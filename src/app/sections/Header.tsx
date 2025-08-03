9a23ce296583c7d1b8bc094461abe5b1d48023af
anandureghu <anandukr145@gmail.com>
2025-08-03 14:41:12 +0530

    initial commit

---
+++ src/app/sections/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-screen flex items-center justify-center text-5xl">
+      GSAP Learning
+    </div>
+  );
+};
+
+export default Header;
